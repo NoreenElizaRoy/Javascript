88c6461ddf1690153de57bf4be33b2dae06f0e0b
NOREEN ELIZA ROY <NoreenElizaRoy>
2026-06-03 16:06:46 +0530

    javascripttask1

---
+++ task.js
@@ -0,0 +1,132 @@
+//sum
+var a=10
+var b=20
+var sum=a+b
+console.log(sum)
+
+//areA
+let length=20
+let breadth=20
+let area=length*breadth
+console.log(area)
+
+//Area and circumference of circle
+const pi=3.14
+let r=10
+let areac=pi*r**2
+console.log("Area of circle=",areac)
+
+let cir=2*pi*r
+console.log("Circumference is =",cir)
+
+//Average of three numbers
+let num1=20
+let num2=30
+let num3=40
+let avg=(num1+num2+num3)/3
+console.log("Average of (20,30,40)=",avg)
+
+//simple Interest
+var p=1000
+var rate=5
+var t=2
+let si=(p*rate*t)/100
+console.log("Simple interest =",si)
+
+//Celsius to Fahrenheit
+let celsius=25
+let f=(celsius*(9/5)+32)
+console.log("Fahrenheit temperature=",f)
+
+//Square and cube
+let x=3
+let square=Math.pow(x,2)
+let cube=Math.pow(x,3)
+console.log("Square=",square,"Cube =",cube)
+
+//5 subject marks total and percentage
+let m1=85
+let m2=90
+let m3=78
+let m4=88
+let m5=92
+let tm=m1+m2+m3+m4+m5
+console.log("Total Marks=",tm)
+
+let tp=(tm/500)*100
+console.log("Percentage of marks=",tp+"%")
+
+//Price and Discount
+let price=1000
+let disPercent=20
+
+let disamt=(price*20)/100
+let final=price-disamt
+
+console.log("Final price=",final)
+
+//Declare the base and height of a triangle and calculate its area.
+let base=10
+let height=8
+let tri=((1/2)*base*height)
+console.log("Area of triange=",tri)
+
+//Declare the side of a square and calculate its perimeter.
+let side=4
+let perimeter=4*side
+console.log("Perimeter of square=",perimeter)
+
+//Declare a number and determine the remainder when divided by another number.
+let num=17
+let div=5
+let remainder=num%div
+console.log("Remainder=",remainder)
+
+//Declare a distance in kilometers and convert it to meters.
+let km=5
+let m=5*1000
+console.log("Distance in meter=",m)
+
+//age affter 10 
+let age=10
+let fage=age+10
+console.log("Age=",fage)
+
+//total bill
+let item1 = 250;
+let item2 = 150;
+let item3 = 300;
+
+let totalBill = item1 + item2 + item3;
+
+console.log("Total Bill Amount =", totalBill);
+
+//number half,double and triple
+let number = 12;
+
+let half = number / 2;
+let double = number * 2;
+let triple = number * 3;
+
+console.log("Half =", half);
+console.log("Double =", double);
+console.log("Triple =", triple);
+
+//room dimensions
+let lengthr = 10;
+let widthr = 5;
+
+let arear = lengthr * widthr;
+let perimeterrm = 2 * (lengthr + widthr);
+
+console.log("Area =", arear);
+console.log("Perimeter =", perimeterrm,);
+
+//profit
+let costPrice = 800;
+let sellingPrice = 1000;
+
+let profit = sellingPrice - costPrice;
+
+console.log("Profit =", profit);
+
