dce9b16ae717fd937edc507fea7b38834279bc9b
NOREEN ELIZA ROY <NoreenElizaRoy>
2026-06-04 10:29:30 +0530

    task2

--- task2.js
+++ task2.js
@@ -1,61 +1,61 @@
 //Create a variable for your name and print its data type.
-var name="noreen"
-console.log(typeof name)
+var name="noreen";
+console.log(typeof name);
 
 //Create a variable for your age and print its data type.
-var age=22
-console.log(typeof age)
+var age=22;
+console.log(typeof age);
 
 //Create a variable for your monthly salary and print its data type.
-const salary=15000
-console.log(typeof salary)
+const salary=15000;
+console.log(typeof salary);
 
 //Create a variable without a value and check its type.
-let a
-console.log(typeof a )
+let a;
+console.log(typeof a );
 
 //Create a variable with null and check its type.
-let b=null
-console.log(typeof b )
+let b=null;
+console.log(typeof b );
 
 //Store a phone number and determine the best data type for it.
-let phnnumber=8457545752
-console.log(typeof phnnumber)
+let phnnumber=8457545752;
+console.log(typeof phnnumber);
 
 //Create 10 variables with different data types and print them.
-name="noreen"
-age=22
-a
-b=null
-let isEmp=false
+name="noreen";
+age=22;
+a;
+b=null;
+let isEmp=false;
 let bigNum = 1234567890123456789n;
-let unique=Symbol("id")
-let fruits=["apple","orange"]
-let person={name:"noreen"}
-console.log(typeof name)
-console.log(typeof age)
-console.log(typeof a1)
-console.log(typeof b)
-console.log(typeof isEmp)
-console.log(typeof bigNum)
-console.log(typeof unique)
-console.log(typeof fruits)
-console.log(typeof person)
+let unique=Symbol("id");
+let fruits=["apple","orange"];
+let person={name:"noreen"};
+console.log(typeof name);
+console.log(typeof age);
+console.log(typeof a1);
+console.log(typeof b);
+console.log(typeof isEmp);
+console.log(typeof bigNum);
+console.log(typeof unique);
+console.log(typeof fruits);
+console.log(typeof person);
 
 //mini user profile
-name="Noreen"
-age=22
-phnnum=8547545752
-const mail="noreen@gmail.com"
-let hobbies=["Coding","Dancing"]
-console.log("MINI USER PROFILE")
-console.log("NAME :",name)
-console.log("AGE :",age)
-console.log("PHONENUMBER :",phnnum)
-console.log("EMAIL :",mail)
-console.log("HOBBIES :",hobbies)
+name="Noreen";
+age=22;
+phnnum=8547545752;
+const mail="noreen@gmail.com";
+let hobbies=["Coding","Dancing"];
+console.log("MINI USER PROFILE");
+console.log("NAME :",name);
+console.log("AGE :",age);
+console.log("PHONENUMBER :",phnnum);
+console.log("EMAIL :",mail);
+console.log("HOBBIES :",hobbies);
 
-console.log("Find the difference between null and undefined using examples.")
+console.log("Find the difference between null and undefined using examples.");
 let x;
 
 console.log(x);                     //undefined
